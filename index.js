aa9ddecab7e8fa5e430f60ee4071231e23a01b3f
Rafael Mesquita <rgalvaomesquita@gmail.com>
2020-03-03 12:10:13 -0300

    Fixing public path

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','ejs')
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname,'public')))
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/',async(request,response) => {
