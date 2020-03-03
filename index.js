ff149e432576ba9134f1c25b4f6fec6dfe118896
Rafael Mesquita <rgalvaomesquita@gmail.com>
2020-03-03 11:57:08 -0300

    fix path

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-
+const path = require('path')
 const sqlite = require('sqlite')
-const dbConnection = sqlite.open('banco.sqlite',{Promise})
+const dbConnection = sqlite.open(path.resolve(__dirname,'banco.sqlite'),{Promise})
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000
 app.set('view engine','ejs')
